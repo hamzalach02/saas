af08a9709d75f52a0d1e1f99abb30da9512d0ce6
hamzalach02 <lachhamza02@gmail.com>
2024-09-08 16:05:19 +0100

    commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/profile","/freeplan",'/choose','/custom','/GenerateSpring'],
+  matcher: ["/profile","/freeplan",'/choose','/custom','/GenerateSpring','/simple'],
   // matcher: ["/((?!register|api|login).*)"],
 
 };
