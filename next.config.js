369751b429f79bd843b403fb28432a7d0348a5ac
hamzalach02 <lachhamza02@gmail.com>
2024-09-08 14:22:45 +0100

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "**",
-          },
-        ],
-      },
-}
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "**",
+        },
+      ],
+    },
+    experimental: {
+      missingSuspenseWithCSRBailout: false,
+    },
+}
